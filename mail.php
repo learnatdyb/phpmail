d4f6e751e7eecf0d86087c218acaf3e523cd523d
learnatdyb <69023628+learnatdyb@users.noreply.github.com>
2022-03-14 10:32:45 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@ $message= $_POST['message'];
 $to = "smahendrancs@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: smahendrancs@gmail.com";
+$headers = "From: https://learnatdyb.github.io/phpmail/";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
