68d9e822dcd35c211212326bbfaa6f4eddbc1aad
smahendrancs <smahendrancs@gmail.com>
2022-03-14 10:16:41 +0530

    mail

---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "smahendrancs@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@codeconia.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
