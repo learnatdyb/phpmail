1452368d0fd04f2bc94024b3b37e889aa436df04
learnatdyb <69023628+learnatdyb@users.noreply.github.com>
2022-03-14 11:32:40 +0530

    Update subscriberform.php

--- subscriberform.php
+++ subscriberform.php
@@ -1,5 +1,5 @@
 <?php
-if($_POST["Message"]) {
+if($_POST["message"]) {
 mail("smahendrancs@gmail.com", "Here is the sample subject line",
 $_POST["Insert Your Message"]. "From: smahendrancs@gmail.com");
 }
